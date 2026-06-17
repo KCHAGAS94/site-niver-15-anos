c907fd57960718e3f6f5b250d826d7225d5d67da
kenedy <kenedychagas@gmail.com>
2026-06-17 00:42:47 -0300

    feat: icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,7 @@ export const metadata: Metadata = {
   description: 'Celebração dos 15 anos da Vitória - Um momento especial para marcar essa data inesquecível',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/icon.png',
   },
 }
 
